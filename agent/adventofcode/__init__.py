0e1b83a58bba7ab8372caa624532c052f1c89066
Jason Steving <jason@spiralup.co>
2024-11-17 12:49:11 -0800

    Impl Scraping AoC Problems

---
+++ agent/adventofcode/__init__.py
@@ -0,0 +1,4 @@
+from agent.adventofcode.scrape_problems import scrape_aoc
+
+
+__all__ = ["scrape_aoc"]
---
+++ agent/adventofcode/scrape_problems.py
@@ -0,0 +1,57 @@
+import asyncio
+from typing import Literal, cast
+import aiohttp
+from bs4 import BeautifulSoup
+import asyncclick as click
+from asyncclick import Choice
+from os import environ
+
+ProblemPart = Literal[1] | Literal[2]
+
+match environ.get("AOC_COOKIE"):
+    case str(cookie):
+        AOC_COOKIE = cookie
+    case _:
+        raise RuntimeError("You must set the AOC_COOKIE env variable!")
+
+
+async def fetch_problem(
+    session: aiohttp.ClientSession, year: int, day: int, part: ProblemPart
+) -> str:
+    url = f"https://adventofcode.com/{year}/day/{day}"
+    headers = {"Cookie": AOC_COOKIE}
+    async with session.get(url, headers=headers) as response:
+        return await response.text()
+
+
+def parse_problem(html: str, part: ProblemPart):
+    soup = BeautifulSoup(html, "html.parser")
+    article = soup.find_all("article", class_="day-desc")
+    if article:
+        # Let's just return the html as a string for now.
+        return str(article[part - 1])
+        # return article[part - 1].get_text(separator="\n", strip=False)
+    return "Problem description not found."
+
+
+async def scrape_aoc(year: int, day: int, part: ProblemPart) -> str:
+    async with aiohttp.ClientSession() as session:
+        html = await fetch_problem(session, year=year, day=day, part=part)
+
+    return parse_problem(html, part=part)
+
+
+@click.command()
+@click.option("--year", required=True)
+@click.option("--day", required=True)
+@click.option("--part", type=Choice(["1", "2"]), default="1")
+async def _cmd(
+    year: int,
+    day: int,
+    part: str,  # type: ignore - Need to redeclare with a cast after parsing into an int.
+) -> None:
+    print(await scrape_aoc(year=year, day=day, part=cast(ProblemPart, int(part))))
+
+
+if __name__ == "__main__":
+    asyncio.run(_cmd())
