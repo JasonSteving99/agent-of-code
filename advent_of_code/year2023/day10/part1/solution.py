fb3a5d9804d3f0f64ac9d520e3d066ad0eff15f6
JasonSteving99 <jasonmail99@gmail.com>
2024-11-27 01:12:31 -0800

    Coding-Agent (2023.10.1): Initial Attempt

---
+++ advent_of_code/year2023/day10/part1/solution.py
@@ -0,0 +1,39 @@
+from typing import List
+
+def solve(maze: List[str]) -> int:
+    rows = len(maze)
+    cols = len(maze[0])
+    start = None
+    for r in range(rows):
+        for c in range(cols):
+            if maze[r][c] == 'S':
+                start = (r, c)
+                break
+        if start is not None:
+            break
+
+    q = [(start, 0)]
+    visited = {start}
+    max_dist = 0
+
+    while q:
+        (r, c), dist = q.pop(0)
+        max_dist = max(max_dist, dist)
+
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visited and maze[nr][nc] != '.':
+                visited.add((nr, nc))
+                q.append(((nr, nc), dist + 1))
+
+    return max_dist
+
+def solution() -> int:
+    maze = []
+    while True:
+        try:
+            line = input()
+            maze.append(line)
+        except EOFError:
+            break
+    return solve(maze)
---
+++ advent_of_code/year2023/day10/part1/tests.py
@@ -0,0 +1,30 @@
+# This test suite covers calculating the maximum number of steps to reach any point in a continuous pipe loop starting from 'S'.
+
+from solution import max_steps_in_pipe_loop
+import pytest
+
+@pytest.mark.parametrize("input_maze, expected_steps", [
+    ("......
+.F-7.
+.|.|.
+.L-J.
+.....", 4),
+    ("-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF", 4),
+    ("..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...", 8),
+    ("7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ", 8),
+])
+def test_max_steps_in_pipe_loop(input_maze, expected_steps):
+    actual_steps = max_steps_in_pipe_loop(input_maze)
+    assert actual_steps == expected_steps, f"For the maze:\n{input_maze}\nExpected max steps: {expected_steps}, but got: {actual_steps}"
