e54764ad9be03a869a6be4d5fa5883be9caf595d
JasonSteving99 <jasonmail99@gmail.com>
2024-11-27 01:25:03 -0800

    Coding-Agent (2023.10.1): Initial Attempt

--- advent_of_code/year2023/day10/part1/solution.py
+++ advent_of_code/year2023/day10/part1/solution.py
@@ -1,50 +1,46 @@
 from typing import List
 
-def max_loop_distance(maze: str) -> int:
-    maze_lines = maze.splitlines()
-    rows = len(maze_lines)
-    cols = len(maze_lines[0])
 
-    start_row, start_col = -1, -1
+def solve(maze: List[str]) -> int:
+    rows = len(maze)
+    cols = len(maze[0])
+    start = None
     for r in range(rows):
         for c in range(cols):
-            if maze_lines[r][c] == 'S':
-                start_row, start_col = r, c
+            if maze[r][c] == 'S':
+                start = (r, c)
                 break
-        if start_row != -1:
+        if start is not None:
             break
 
-    if start_row == -1:
-        return 0
+    directions = {'|': ((-1, 0), (1, 0)), '-': ((0, -1), (0, 1)), 'L': ((-1, 0), (0, 1)),
+                  'J': ((-1, 0), (0, -1)), '7': ((1, 0), (0, -1)), 'F': ((1, 0), (0, 1))}
 
     def get_neighbors(r, c):
-        neighbors = []
-        possible_pipes = []
-        if maze_lines[r][c] == 'S':
-            possible_pipes = ['-', '|', 'L', 'J', '7', 'F']
-        elif maze_lines[r][c] == '-':
-            possible_pipes = ['-', 'L', 'J', '7', 'F']
-        elif maze_lines[r][c] == '|':
-            possible_pipes = ['|', 'L', 'J', '7', 'F']
-        elif maze_lines[r][c] in ('L','J','7','F'):
-            possible_pipes = ['-', '|', 'L', 'J', '7', 'F']            
+        if maze[r][c] == 'S':
+            possible_directions = []
+            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:  # Explore all 4 directions
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and maze[nr][nc] != '.':
+                    possible_directions.append((dr, dc))
 
-        for dr, dc, char in [(0, 1, '-'), (0, -1, '-'), (1, 0, '|'), (-1, 0, '|')]:
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < rows and 0 <= nc < cols:
-                neighbor_char = maze_lines[nr][nc]
-                if neighbor_char != '.':
-                    if (char == '-' and neighbor_char in possible_pipes and (neighbor_char == '-' or
-                        (neighbor_char == 'L' and dc == 1) or (neighbor_char == 'J' and dc == -1) or
-                        (neighbor_char == '7' and dr == 1) or (neighbor_char == 'F' and dr == 1))) or \
-                    (char == '|' and neighbor_char in possible_pipes and (neighbor_char == '|' or
-                        (neighbor_char == 'L' and dc == -1) or (neighbor_char == 'J' and dc == 1) or
-                        (neighbor_char == '7' and dr == -1) or (neighbor_char == 'F' and dc == -1))):
-                       neighbors.append((nr, nc))
-        return neighbors
+            valid_neighbors = []
+            for dr1, dc1 in possible_directions:
+                for dr2, dc2 in possible_directions:
+                    if (dr1, dc1) != (dr2, dc2) and (dr1 + dr2, dc1 + dc2) == (0, 0):  # Check for opposite direction
+                        valid_neighbors = [(r + dr1, c + dc1), (r + dr2, c + dc2)]
+                        return valid_neighbors
 
-    q = [((start_row, start_col), 0)]
-    visited = {(start_row, start_col)}
+        else:
+            neighbors = []
+            for dr, dc in directions[maze[r][c]]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and maze[nr][nc] != '.':
+                    neighbors.append((nr, nc))
+            return neighbors
+
+    q = [(start, 0)]
+    visited = {start}
     max_dist = 0
 
     while q:
@@ -54,16 +50,17 @@ def max_loop_distance(maze: str) -> int:
             if (nr, nc) not in visited:
                 visited.add((nr, nc))
                 q.append(((nr, nc), dist + 1))
-
     return max_dist
 
+
 def solution() -> int:
-    maze_str: str = ""
+    maze_lines: List[str] = []
     while True:
         try:
             line = input()
-            maze_str += line + "\n"
+            maze_lines.append(line)
         except EOFError:
             break
 
-    return max_loop_distance(maze_str)
+    result = solve(maze_lines)
+    return result
--- advent_of_code/year2023/day10/part1/tests.py
+++ advent_of_code/year2023/day10/part1/tests.py
@@ -1,17 +1,9 @@
+# These tests cover the calculation of the maximum number of steps required to reach the furthest point within a pipe loop starting from 'S', considering different maze configurations.
+
+from solution import max_steps_in_loop
 import pytest
-from solution import max_loop_distance
 
-# PROBLEM STATEMENT:
-# Given a pipe maze represented by a grid of characters, find the maximum distance 
-# reachable from the starting point 'S' within the main loop of the pipes.
-
-
-@pytest.mark.parametrize("maze_str, expected_distance", [
-    (".....\n.F-7.\n.|.|.\n.L-J.\n.....", 4),
-    ("-L|F7\n7S-7|\nL|7||\n-L-J|\nL|-JF", 4),
-    ("..F7.\n.FJ|.\nSJ.L7\n|F--J\nLJ...", 8),
-    ("7-F7-\n.FJ|7\nSJLL7\n|F--J\nLJ.LJ", 8),
-])
-def test_max_loop_distance(maze_str, expected_distance):
-    actual_distance = max_loop_distance(maze_str)
-    assert actual_distance == expected_distance, f"Input: {maze_str}, Expected: {expected_distance}, Actual: {actual_distance}"
+@pytest.mark.parametrize("maze, expected_steps", [(".....\n.F-7.\n.|.|.\n.L-J.\n.....", 4), ("-L|F7\n7S-7|\nL|7||\n-L-J|\nL|-JF", 4), ("..F7.\n.FJ|.\nSJ.L7\n|F--J\nLJ...", 8), ("7-F7-\n.FJ|7\nSJLL7\n|F--J\nLJ.LJ", 8)])
+def test_max_steps_in_loop(maze, expected_steps):
+    actual_steps = max_steps_in_loop(maze)
+    assert actual_steps == expected_steps, f"For the maze:\n{maze}, expected max steps to be {expected_steps}, but got {actual_steps}"
