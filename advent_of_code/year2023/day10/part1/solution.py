b5eb9a946f4c047084edeb57595fba7736e85e7d
JasonSteving99 <jasonmail99@gmail.com>
2024-11-27 01:32:05 -0800

    Coding-Agent (2023.10.1): Unit Test Failure Fixes (#6)
    ### Addressing the following unit test failures:
    ```json
    {
        "err_msg": "Unit Test Results: 3 of 3 Failed \n\n\n### test_max_loop_distance[.....\\n.F-7.\\n.|.|.\\n.L-J.\\n.....-4] at line 5\nmaze = '.....\\n.F-7.\\n.|.|.\\n.L-J.\\n.....', expected_distance = 4\n\n    @pytest.mark.parametrize(\"maze, expected_distance\", [\n        (\".....\\n.F-7.\\n.|.|.\\n.L-J.\\n.....\", 4),\n        (\"7-F7-\\n.FJ|7\\nSJLL7\\n|F--J\\nLJ.LJ\", 8),\n        (\"..F7.\\n.FJ|.\\nSJ.L7\\n|F--J\\nLJ...\", 8),\n    ])\n    def test_max_loop_distance(maze, expected_distance):\n        actual_distance = max_loop_distance(maze)\n>       assert actual_distance == expected_distance, f\"For the maze:\\n{maze}\\nExpected max distance: {expected_distance}, but got {actual_distance}\"\nE       AssertionError: For the maze:\nE         .....\nE         .F-7.\nE         .|.|.\nE         .L-J.\nE         .....\nE         Expected max distance: 4, but got 0\nE       assert 0 == 4\n\nadvent_of_code/year2023/day10/part1/tests.py:13: AssertionError\n\n\n\n\n### test_max_loop_distance[7-F7-\\n.FJ|7\\nSJLL7\\n|F--J\\nLJ.LJ-8] at line 5\nmaze = '7-F7-\\n.FJ|7\\nSJLL7\\n|F--J\\nLJ.LJ', expected_distance = 8\n\n    @pytest.mark.parametrize(\"maze, expected_distance\", [\n        (\".....\\n.F-7.\\n.|.|.\\n.L-J.\\n.....\", 4),\n        (\"7-F7-\\n.FJ|7\\nSJLL7\\n|F--J\\nLJ.LJ\", 8),\n        (\"..F7.\\n.FJ|.\\nSJ.L7\\n|F--J\\nLJ...\", 8),\n    ])\n    def test_max_loop_distance(maze, expected_distance):\n        actual_distance = max_loop_distance(maze)\n>       assert actual_distance == expected_distance, f\"For the maze:\\n{maze}\\nExpected max distance: {expected_distance}, but got {actual_distance}\"\nE       AssertionError: For the maze:\nE         7-F7-\nE         .FJ|7\nE         SJLL7\nE         |F--J\nE         LJ.LJ\nE         Expected max distance: 8, but got 6\nE       assert 6 == 8\n\nadvent_of_code/year2023/day10/part1/tests.py:13: AssertionError\n\n\n\n\n### test_max_loop_distance[..F7.\\n.FJ|.\\nSJ.L7\\n|F--J\\nLJ...-8] at line 5\nmaze = '..F7.\\n.FJ|.\\nSJ.L7\\n|F--J\\nLJ...', expected_distance = 8\n\n    @pytest.mark.parametrize(\"maze, expected_distance\", [\n        (\".....\\n.F-7.\\n.|.|.\\n.L-J.\\n.....\", 4),\n        (\"7-F7-\\n.FJ|7\\nSJLL7\\n|F--J\\nLJ.LJ\", 8),\n        (\"..F7.\\n.FJ|.\\nSJ.L7\\n|F--J\\nLJ...\", 8),\n    ])\n    def test_max_loop_distance(maze, expected_distance):\n        actual_distance = max_loop_distance(maze)\n>       assert actual_distance == expected_distance, f\"For the maze:\\n{maze}\\nExpected max distance: {expected_distance}, but got {actual_distance}\"\nE       AssertionError: For the maze:\nE         ..F7.\nE         .FJ|.\nE         SJ.L7\nE         |F--J\nE         LJ...\nE         Expected max distance: 8, but got 6\nE       assert 6 == 8\n\nadvent_of_code/year2023/day10/part1/tests.py:13: AssertionError\n\n\n"
    }
    ```
    
    ### Theorized solution:
    ```json
    {
        "problem_explanation": "The implementation does not correctly traverse the loop, and instead treats the maze as a grid that it can traverse without limitations on which neighbors are accessible from the current grid cell.  Thus, the calculated distances are incorrect as any non-'.' neighbor is considered reachable.",
        "optional_theorized_unit_test_fix": null,
        "optional_theorized_implementation_fix": "The issue lies in the `get_neighbors` function. It checks for neighbors based solely on whether a tile is not ground (`.`).  This is incorrect as it doesn't consider the pipe connections. For example, a vertical pipe '|' should only have neighbors above and below it, but the current implementation might consider horizontal neighbors if they are not ground tiles. The solution is to add logic within `get_neighbors` to filter valid connections based on the pipe types ('|', '-', 'L', 'J', '7', 'F', 'S') of both the current tile and its potential neighbors."
    }
    ```

--- advent_of_code/year2023/day10/part1/solution.py
+++ advent_of_code/year2023/day10/part1/solution.py
@@ -17,14 +17,30 @@ def solve(maze: List[str]) -> int:
 
     def get_neighbors(r, c):
         neighbors = []
+        tile = maze[r][c]
+        if tile == 'S':
+            tile = '|'
+        
         if r > 0 and maze[r-1][c] != '.':
-            neighbors.append((r-1, c))
+            neighbor_tile = maze[r-1][c]
+            if neighbor_tile == 'S': neighbor_tile = '|'
+            if (tile in '|7FJ' and neighbor_tile in '|LJ7') or (neighbor_tile in '|7FJ' and tile in '|LJ7'):
+                neighbors.append((r - 1, c))
         if r < rows - 1 and maze[r+1][c] != '.':
-            neighbors.append((r+1, c))
+            neighbor_tile = maze[r+1][c]
+            if neighbor_tile == 'S': neighbor_tile = '|'
+            if (tile in '|LF' and neighbor_tile in '|7LF') or (neighbor_tile in '|LF' and tile in '|7LF'):
+                neighbors.append((r + 1, c))
         if c > 0 and maze[r][c-1] != '.':
-            neighbors.append((r, c-1))
+            neighbor_tile = maze[r][c-1]
+            if neighbor_tile == 'S': neighbor_tile = '|'
+            if (tile in '-LJ7' and neighbor_tile in '-7FJ') or (neighbor_tile in '-LJ7' and tile in '-7FJ'):
+                neighbors.append((r, c - 1))
         if c < cols - 1 and maze[r][c+1] != '.':
-            neighbors.append((r, c+1))
+            neighbor_tile = maze[r][c+1]
+            if neighbor_tile == 'S': neighbor_tile = '|'
+            if (tile in '-LF' and neighbor_tile in '-LJF') or (neighbor_tile in '-LF' and tile in '-LJF'):
+                neighbors.append((r, c + 1))
         return neighbors
 
     q = [(start, 0)]
